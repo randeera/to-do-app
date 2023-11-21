25a9a2b9ae487a23affcd0b868a424bef041cd06
Kaveendra Randeera <khkrandeera@gmail.com>
2023-11-21 12:32:48 +0530

    Finialize the structure of the web services

--- back-end/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
+++ back-end/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
@@ -1,9 +1,9 @@
 package lk.ijse.dep11.todo.api;
 
 import lk.ijse.dep11.todo.to.TaskTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,28 +11,36 @@ import java.util.Map;
 @RequestMapping("/tasks")
 public class TaskHttpController {
 
-    @PostMapping
-    public TaskTO createTask(@RequestBody String description) {
+    /*
+        The schema object defines the content of the request and response. In other words,
+        it refers to the definition and set of rules (validation rules) for representing
+        the structure of API data
+    */
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public TaskTO createTask(@RequestBody TaskTO task) {
         System.out.println("createTask()");
-        return new TaskTO(1, description, false);
+        return null;
     }
 
-    @GetMapping
-    public List<TaskTO> getAllTasks() {
-        System.out.println("getAllTasks()");
-        return new ArrayList<>();
-    }
-
-    // PATCH /tasks/{id}
-    @PatchMapping("/{id}")
-    public void updateTask(@PathVariable("id") String taskId,
-                           @RequestBody Map<String, String> params) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "/{id}", consumes = "application/json")
+    public void updateTask(@PathVariable String id,
+                           @RequestBody TaskTO task) {
         System.out.println("updateTask()");
     }
 
-    // DELETE /tasks/{id}
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable String id) {
+    public void deleteTask(@PathVariable("id") String taskId) {
         System.out.println("deleteTask()");
+    }
+
+    /* @ResponseStatus(HttpStatus.OK) */
+    @GetMapping(produces = "application/json")
+    public List<TaskTO> getAllTasks() {
+        System.out.println("getAllTasks()");
+        return null;
     }
 }

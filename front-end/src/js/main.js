9501a292b167171a8ef78bc8642978cd7bea8167
Kaveendra Randeera <khkrandeera@gmail.com>
2023-11-22 12:06:22 +0530

    Consume Load, Delete Task Web Services

--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -5,19 +5,21 @@ const taskContainerElm = document.querySelector("#task-container");
 loadAllTasks();
 
 function loadAllTasks(){
-    // Todo: Retrive all tasks from the back-end
-    const taskList = [
-        {id: 1, description: 'Task 1', status: false},
-        {id: 2, description: 'Task 2', status: true},
-        {id: 3, description: 'Task 3', status: true},
-        {id: 4, description: 'Task 4', status: false},
-    ];
-    taskList.forEach(task => createTask(task));
+    fetch('http://localhost:8080/tasks').then(res => {
+        if (res.ok){
+            res.json().then(taskList => taskList.forEach(task => createTask(task))) ;
+        }else{
+            alert("Failed to load task list");
+        }
+    }).catch(err => {
+        alert("Something went wrong, try again later");
+    });
 }
 
 function createTask(task){
     const liElm = document.createElement('li');
     taskContainerElm.append(liElm);
+    liElm.id = "task-" + task.id;
 
     liElm.innerHTML = `
         <input id="chk-task-${task.id}" type="checkbox" ${task.status ? "checked": ""}>
@@ -28,9 +30,20 @@ function createTask(task){
 
 taskContainerElm.addEventListener('click', (e)=>{
     if (e.target?.classList.contains('delete')){
-        // Todo: Delete the task from the back-end
-        // If success
-        e.target.closest("li").remove();
+
+        const taskId = e.target.closest('li').id.substring(5);
+
+        fetch(`http://localhost:8080/tasks/${taskId}`, {method: 'DELETE'})
+        .then(res => {
+            if (res.ok){
+                e.target.closest("li").remove();
+            }else{
+                alert("Failed to delete the task");
+            }
+        }).catch(err => {
+            alert("Something went wrong, try again later");
+        });  
+              
     } else if (e.target?.tagName === "INPUT"){
         // Todo: Update the task status in the back-end
         // If not success
